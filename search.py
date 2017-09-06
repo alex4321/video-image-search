4d8ab5e554d7aa200144bf3bdfa1eaf38b32a5af
alexp <Djang12346>
2017-09-07 01:21:29 +0300

    Search algorythm fix

--- search.py
+++ search.py
@@ -20,23 +20,21 @@ def search(image, phash_indexes, hist_indexes):
     ])
     similar_phash_indexes = np.argsort(phash_distances)[:CHECK_NEAREST_PHASHES]
 
-    hist_indices_to_sort = []
+    result = []
     for i in similar_phash_indexes:
         fname, frame = phash_indexes[i].fname, phash_indexes[i].frame
         hist_indexes = fname_hist_indexes[fname]
         frame_distances = np.abs(np.array([index.frame for index in hist_indexes]) - frame)
         nearest_frame_indexes = frame_distances.argsort()[:2]
-        hist_indices_to_sort.append(hist_indexes[nearest_frame_indexes[0]])
-        hist_indices_to_sort.append(hist_indexes[nearest_frame_indexes[1]])
-
-    hist_distances = np.array([
-        hist_dissimilarity(image_hist, index.value)
-        for index in hist_indices_to_sort
-    ])
-    hist_indexes = hist_distances.argsort()
-    result = []
-    for index in hist_indexes:
-        item = (hist_indices_to_sort[index].fname, hist_indices_to_sort[index].frame)
-        if item not in result:
-            result.append(item)
+        hist_indices_to_sort = [hist_indexes[nearest_frame_indexes[0]],
+                                hist_indexes[nearest_frame_indexes[1]]]
+        hist_distances = np.array([
+            hist_dissimilarity(image_hist, index.value)
+            for index in hist_indices_to_sort
+        ])
+        hist_indexes = hist_distances.argsort()
+        for index in hist_indexes:
+            item = (hist_indices_to_sort[index].fname, hist_indices_to_sort[index].frame)
+            if item not in result:
+                result.append(item)
     return result
